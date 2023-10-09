66f133e55ade4d44c7d6f8dc8f0627413ff1bb1d
GabrielBifon05 <gabriel.bsouza@sptech.school>
2023-10-09 07:01:39 -0300

    Atualização de Inserts

--- bancoDados/Script-Inserts-GCT.sql
+++ bancoDados/Script-Inserts-GCT.sql
@@ -1,5 +1,5 @@
 -- Cadastro de Empresas
-INSERT INTO `Empresa` (`razao_social`, `cnpj`, `numero_imovel`, `cep`, `email`, `telefone`) 
+INSERT INTO `empresa` (`razao_social`, `cnpj`, `numero_imovel`, `cep`, `email`, `telefone`) 
 VALUES  ('Pague Seguro',"61.186.888/0093-01", 763, '09260-640', 'pagueSeguro@gmail.com', '123123123')
 	, ('SPTECH', "61.186.888/0093-01", 298,'09260-640', 'sptech@gmail.com', '456456456')
 	, ('Amazon', "61.186.888/0093-01", 234,'09260-640', 'amazon@gmail.com', '889877677');
@@ -7,19 +7,19 @@ Select * from `Empresa`;
 
 
 -- Cadastro de Gerentes
-INSERT INTO `Funcionario` (`nome`, `email`, `senha`, `cargo`, `cpf`, `permissao`, `fk_gerente`, `fk_empresa`)
+INSERT INTO `funcionario` (`nome`, `email`, `senha`, `cargo`, `cpf`, `permissao`, `fk_gerente`, `fk_empresa`)
 VALUES ('Gabriel', 'gabriel@gmail.com', '12345', 'Gerente', '690.969.360-43', '1', null, 1)
 	, ('Fernando Brandão', 'fernado@gmail.com', '12345', 'Presidente', '226.146.010-47', '1', null, 2)
     , ('Cláudio', 'claudio@gmail.com', '12345', 'Gerente', '810.791.190-35', '1', null, 3);
 
 
 -- Cadastro de Funcionários
-INSERT INTO `Funcionario` (`nome`, `email`, `senha`, `cargo`, `cpf`, `permissao`, `fk_gerente`, `fk_empresa`)
+INSERT INTO `funcionario` (`nome`, `email`, `senha`, `cargo`, `cpf`, `permissao`, `fk_gerente`, `fk_empresa`)
 values ('Cleiton Rodrigues', 'cleiton@gmail.com', '12345', 'Analísta Junior', "514.184.580-07", '2', 1, 1)
 	, ('Carlos Souza', 'carlos@gmail.com', '12345', 'Analísta Sênior', "541.886.660-56", '1', 2, 2)
     , ('Pedro Henrique', 'pedro@gmail.com', '12345', 'Analísta Sênior', "091.045.750-67", '1', 3, 3);
 
-SELECT * FROM `Funcionario`;
+SELECT * FROM `funcionario`;
 
 
 -- Cadastro de Servidores 
@@ -27,4 +27,6 @@ INSERT INTO `servidor` (`nome`, `codigo`, `tipo`, `descricao`, `fk_empresa`)
 VALUES ('SERVER-AHRL1NB', 'XPTO-0987', 'Servidor Principal', 'Servidor responsável por executar X tarefa', 1)
 	, ('SERVER-9HJD2AL', 'XP-9384', 'Servidor de Backup', 'Servidor responsável por backups', 1)
     , ('SERVER-UHD71P6', 'LOC-0284', 'Servidor de Homologação', 'Servidor responsável por Homologações ', 1);
+
+
     
--- bancoDados/script_banco_v5_26-setembro.sql
+++ bancoDados/script_banco_v5_26-setembro.sql
@@ -91,7 +91,7 @@ CREATE TABLE IF NOT EXISTS `registro` (
 );
 
 -- Cadastro de Empresas
-INSERT INTO `Empresa` (`razao_social`, `cnpj`, `numero_imovel`, `cep`, `email`, `telefone`) 
+INSERT INTO `empresa` (`razao_social`, `cnpj`, `numero_imovel`, `cep`, `email`, `telefone`) 
 VALUES ('Pague Seguro',"61.186.888/0093-01", 763, '09260-640', 'pagueSeguro@gmail.com', '123123123')
 	, ('SPTECH', "61.186.888/0093-01", 298,'09260-640', 'sptech@gmail.com', '456456456')
 	, ('Amazon', "61.186.888/0093-01", 234,'09260-640', 'amazon@gmail.com', '889877677');
@@ -99,14 +99,14 @@ Select * from `Empresa`;
 
 
 -- Cadastro de Gerentes
-INSERT INTO `Funcionario` (`nome`, `email`, `senha`, `cargo`, `cpf`, `permissao`, `fk_gerente`, `fk_empresa`)
+INSERT INTO `funcionario` (`nome`, `email`, `senha`, `cargo`, `cpf`, `permissao`, `fk_gerente`, `fk_empresa`)
 VALUES ('Gabriel', 'gabriel@gmail.com', '12345', 'Gerente', '690.969.360-43', '1', null, 1)
 	, ('Fernando Brandão', 'fernado@gmail.com', '12345', 'Presidente', '226.146.010-47', '1', null, 2)
     , ('Cláudio', 'claudio@gmail.com', '12345', 'Gerente', '810.791.190-35', '1', null, 3);
 
 
 -- Cadastro de Funcionários
-INSERT INTO `Funcionario` (`nome`, `email`, `senha`, `cargo`, `cpf`, `permissao`, `fk_gerente`, `fk_empresa`)
+INSERT INTO `funcionario` (`nome`, `email`, `senha`, `cargo`, `cpf`, `permissao`, `fk_gerente`, `fk_empresa`)
 values ('Cleiton Rodrigues', 'cleiton@gmail.com', '12345', 'Analísta Junior', "514.184.580-07", '2', 1, 1)
 	, ('Carlos Souza', 'carlos@gmail.com', '12345', 'Analísta Sênior', "541.886.660-56", '1', 2, 2)
     , ('Pedro Henrique', 'pedro@gmail.com', '12345', 'Analísta Sênior', "091.045.750-67", '1', 3, 3);
@@ -118,3 +118,53 @@ INSERT INTO `servidor` (`nome`, `codigo`, `tipo`, `descricao`, `fk_empresa`)
 VALUES ('SERVER-AHRL1NB', 'XPTO-0987', 'Servidor Principal', 'Servidor responsável por executar X tarefa', 1)
 	, ('SERVER-9HJD2AL', 'XP-9384', 'Servidor de Backup', 'Servidor responsável por backups', 1)
     , ('SERVER-UHD71P6', 'LOC-0284', 'Servidor de Homologação', 'Servidor responsável por Homologações ', 1);
+
+
+-- Cadastro de Componentes
+INSERT INTO `componente` (`tipo_componente`) VALUES 
+('CPU'),
+('RAM'),
+('Disco');
+
+
+
+-- Cadastro de Modelo de Componentes
+INSERT INTO `modelo_componente` (`modelo`, `fabricante`, `fk_servidor`, `fk_componente`, `metrica_maxima`)
+VALUES
+('Intel Core i7 13700K', 'Intel', 1, 1, '70'),
+('AMD Ryzen 7 7800X', 'AMD', 1, 1, '70'),
+('Apple M1 Max', 'Apple', 1, 1, '70'),
+('DDR4 3200MHz 16GB', 'Corsair', 1, 2, '70'),
+('DDR5 4800MHz 32GB', 'G.Skill', 1, 2, '70'),
+('LPDDR5 6400MHz 64GB', 'Samsung', 1, 2, '70'),
+('Samsung 980 Pro 1TB', 'Samsung', 1, 3, '70'),
+('WD Black SN850 2TB', 'Western Digital', 1, 3, '70'),
+('Seagate FireCuda 530 4TB', 'Seagate', 1, 3, '70');
+
+-- Cadastro de Registros (para modelo de componentes)
+--CPU
+INSERT INTO `registro` (`valor_registro`, `data_registro`, `fk_modelo_componente`) VALUES
+(8, `2023/10/09 14:05:32`, 1), --qtdCores
+(16, `2023/10/09 14:05:32`, 1), --qtdThreads
+(10 `2023/10/09 14:05:32`, 1), --temposCpu.user
+(20, `2023/10/09 14:05:32`, 1), --temposCpu.system
+(70, `2023/10/09 14:05:32`, 1), --temposCpu.idle
+(70, `2023/10/09 14:05:32`, 1), --UtilizacaoCore
+(70, `2023/10/09 14:05:32`, 1), --porcentagemUtilizacaoCPU
+(3.5, `2023/10/09 14:05:32`, 1), --frequenciaCpu.current
+(2.5, `2023/10/09 14:05:32`, 1); --freqCpuMin
+
+-- Disco
+INSERT INTO `registro` (`valor_registro`, `data_registro`, `fk_modelo_componente`) VALUES
+(1000, `2023/10/09 14:05:32`, 7), --Quantidade total de massa
+(750, `2023/10/09 14:05:32`, 7), --Quantidade livre de massa
+(250, `2023/10/09 14:05:32`, 7), --Quantidade de massa em uso
+(25, `2023/10/09 14:05:32`, 7); -- Porcentagem em uso
+
+-- RAM
+INSERT INTO `registro` (`valor_registro`, `data_registro`, `fk_modelo_componente`) VALUES
+(16, `2023/10/09 14:05:32`, 4), --ramByteToGigabyteTotal
+(12, `2023/10/09 14:05:32`, 4), --ramByteToGigabyteDisponivel
+(4, `2023/10/09 14:05:32`, 4), --ramByteToGigabyteUsando
+(12, `2023/10/09 14:05:32`, 4), --ramByteToGigabyteLivre
+(25, `2023/10/09 14:05:32`, 4); --ramPercentualUtilizado
